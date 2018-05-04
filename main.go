bab3bca6b4f7498fa169010032211d99621dac78
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-04 19:55:50 -0400

    Initialize router and starting route handlers

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-//"fmt"
-// "github.com/leesper/couchdb-golang"
-// "encoding/json"
-// "strconv"
-//"log"
-//"net/http"
-//"github.com/gorilla/mux"
+import (
+	"fmt"
+	// "github.com/leesper/couchdb-golang"
+	// "encoding/json"
+	// "strconv"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
+	//Init Router
+	fmt.Println("Server starting up...")
+	r := mux.NewRouter()
+
+	//Route Handlers
+	r.HandleFunc("/v1/api/list", getLists).Methods("GET")
+	r.HandleFunc("/v1/api/list/{id}", getList).Methods("GET")
+	r.HandleFunc("/v1/api/list/{id}", createList).Methods("PUT")
+	r.HandleFunc("/v1/api/list/{id}", updateList).Methods("GET")
+	r.HandleFunc("/v1/api/list/{id}", deleteList).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
