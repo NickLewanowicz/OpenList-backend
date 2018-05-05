1c746d8f1cbaef4906037f0f669301e088765e7d
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-04 20:23:14 -0400

    Separated routing to its own file (more scalable)

---
+++ routes.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//Route Struct
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+//Routes Struct
+type Routes []Route
+
+//NewRouter Function
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	Route{
+		"GetLists",
+		"GET",
+		"/v1/api/lists",
+		GetLists,
+	},
+	Route{
+		"GetList",
+		"GET",
+		"/v1/api/lists/{id}",
+		GetList,
+	},
+	Route{
+		"CreateList",
+		"POST",
+		"/v1/api/lists/{id}",
+		CreateList,
+	},
+	Route{
+		"UpdateList",
+		"PUT",
+		"/v1/api/lists/{id}",
+		UpdateList,
+	},
+	Route{
+		"DeleteList",
+		"DELETE",
+		"/v1/api/lists/{id}",
+		DeleteList,
+	},
+}
