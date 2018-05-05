e3898007958199274e56e984429ed2f388a63609
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-04 20:22:47 -0400

    Separated route handlers to their own file

---
+++ handlers.go
@@ -0,0 +1,33 @@
+package main
+
+import "net/http"
+
+//"encoding/json"
+//"fmt"
+// "github.com/leesper/couchdb-golang"
+//"github.com/gorilla/mux"
+
+//GetLists : Get all lists
+func GetLists(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//GetList : Get single list based on ID
+func GetList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//CreateList : Create a list on the DB
+func CreateList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//UpdateList : update the information of a list on the db
+func UpdateList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//DeleteList : delete the list selected from the database
+func DeleteList(w http.ResponseWriter, r *http.Request) {
+
+}
