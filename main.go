9c2bc037f7c79f78a38af738a696d7983f5b510e
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-04 20:24:15 -0400

    Removed old code and now import from list, handlers, and routes

--- main.go
+++ main.go
@@ -2,63 +2,14 @@ package main
 
 import (
 	"fmt"
-	// "github.com/leesper/couchdb-golang"
-	// "encoding/json"
-	// "strconv"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
-
-//List Struct
-type List struct {
-	ID    string `json:"id,omitempty"`
-	Owner string `json:"owner,omitempty"`
-	Title string `json:"title,omitempty"`
-	Date  string `json:"date,omitempty"`
-}
-
-//ListItem Struct
-type ListItem struct {
-	ID   string `json:"id,omitempty"`
-	List string `json:"list,omitempty"`
-	Data string `json:"data,omitempty"`
-}
-
-//DB interaction methods
-func getLists(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func getList(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func createList(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func updateList(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func deleteList(w http.ResponseWriter, r *http.Request) {
-
-}
 
 func main() {
 	//Init Router
 	fmt.Println("Server starting up...")
-	r := mux.NewRouter()
-
-	//Route Handlers
-	r.HandleFunc("/v1/api/list", getLists).Methods("GET")
-	r.HandleFunc("/v1/api/list/{id}", getList).Methods("GET")
-	r.HandleFunc("/v1/api/list/{id}", createList).Methods("POST")
-	r.HandleFunc("/v1/api/list/{id}", updateList).Methods("PUT")
-	r.HandleFunc("/v1/api/list/{id}", deleteList).Methods("DELETE")
+	r := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8000", r))
-
 }
