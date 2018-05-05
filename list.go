761c398fffaac0614cde3fca7114262b1de8c5ea
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-05 10:37:56 -0400

    removed omits to align with MySQL

--- list.go
+++ list.go
@@ -4,16 +4,16 @@ import "time"
 
 //List struct
 type List struct {
-	ID    string     `json:"id,omitempty"`
-	Owner string     `json:"owner,omitempty"`
-	Title string     `json:"title,omitempty"`
+	ID    string     `json:"id"`
+	Owner string     `json:"owner"`
+	Title string     `json:"title"`
 	Date  time.Time  `json:"date"`
 	Items []ListItem `json:"items"`
 }
 
 //ListItem struct
 type ListItem struct {
-	ID   string `json:"id,omitempty"`
-	List string `json:"list,omitempty"`
-	Data string `json:"data,omitempty"`
+	ID   string `json:"id"`
+	List string `json:"list"`
+	Data string `json:"data"`
 }
