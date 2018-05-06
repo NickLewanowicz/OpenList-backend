29d78dbf663fcbc170c3a78eeb4f868a1ca9c9c9
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-05 22:08:30 -0400

    begining of handling POST request for list, json encoding is broken

--- handlers.go
+++ handlers.go
@@ -1,11 +1,12 @@
 package main
 
-import "net/http"
-
-//"encoding/json"
-//"fmt"
-// "github.com/leesper/couchdb-golang"
-//"github.com/gorilla/mux"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
 
 //GetLists : Get all lists
 func GetLists(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +20,23 @@ func GetList(w http.ResponseWriter, r *http.Request) {
 
 //CreateList : Create a list on the DB
 func CreateList(w http.ResponseWriter, r *http.Request) {
+	var list List
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 999999))
+	if err != nil {
+		panic(err)
+	}
 
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &list); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println(body, list)
 }
 
 //UpdateList : update the information of a list on the db
