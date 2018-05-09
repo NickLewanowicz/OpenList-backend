6b3af59e83795b43c128258a1015ecfa52fba90a
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-08 21:38:12 -0400

    Add: Task insert and update functions

--- structs.go
+++ structs.go
@@ -55,6 +55,23 @@ func (u User) updateSQL() {
 	didError(err)
 }
 
+func (t Task) insertSQL() {
+	fmt.Printf("    - Inserting task '" + t.ID + "' into database. ")
+	_, err = db.Exec(fmt.Sprintf("INSERT INTO %s VALUES (%s,%s,%s,%s,%s,%s,%s)", taskTable, t.ID, t.Title, t.Description, t.Created, t.Due, t.Owners, t.Section))
+	didError(err)
+	for i := 0; i < len(t.Owners); i++ {
+		fmt.Printf("    - Inserting task/owner relationship into database. ")
+		_, err = db.Exec(fmt.Sprintf("INSERT INTO %s VALUES (%s,%s)", userTaskTable, t.ID, t.Owners[i].ID))
+		didError(err)
+	}
+}
+
+func (t Task) updateSQL() {
+	fmt.Printf("    - Updating task '" + t.ID + "' in database. ")
+	_, err = db.Exec(fmt.Sprintf("UPDATE %s SET title='%s' description='%s' created='%s' due='%s' WHERE id='%s'", taskTable, t.Title, t.Description, t.Created, t.Due, t.ID))
+	didError(err)
+}
+
 func didError(err error) {
 	if err != nil {
 		fmt.Println("[FAILED]")
