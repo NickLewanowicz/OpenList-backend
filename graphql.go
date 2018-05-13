704cab57dc3005d4ee72e6c9be46247631fee91b
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-13 17:19:04 -0400

    Add: comment  required to build

--- graphql.go
+++ graphql.go
@@ -13,6 +13,7 @@ func GraphQl(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//GraphiQl is the enpoint for exploration of the query schema
 func GraphiQl(w http.ResponseWriter, r *http.Request) {
 	w.Write(page)
 }
